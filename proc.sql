40e8b2458f598eb822bcef13f160fe7f4a04341c
Kyzure <Kyzurene@gmail.com>
2021-10-22 16:22:40 +0800

    Added: Question 2

--- proc.sql
+++ proc.sql
@@ -8,32 +8,41 @@
 
 ---- Non Compliance ----
 CREATE OR REPLACE FUNCTION non_compliance 
-  (IN start_date DATE, IN end_DATE date)
-RETURNS TABLE(eid INTEGER, days INTEGER) AS $$
+  (IN start_date DATE, IN end_date DATE)
+RETURNS TABLE(employee_id INTEGER, number_of_days BIGINT) AS $$
 BEGIN
-  SELECT DISTINCT e.eid, COUNT(*) INTO eid, days
-  FROM Employees e
-  GROUP BY e.eid
-  HAVING NOT EXISTS (
-    SELECT 1
-    FROM HealthDeclaration hd
-    WHERE hd.eid = e.eid 
-      AND hd.date >= start_date
-      AND hd.date <= end_date
-  );
-  RETURN QUERY SELECT eid, days;
+  RETURN QUERY
+    WITH dates (date) AS (
+      SELECT *
+        FROM generate_series(start_date, end_date, '1 day'::interval)
+    )
+    SELECT e.eid, COUNT(*) AS days
+    FROM Employees e, dates d
+    WHERE NOT EXISTS (
+      SELECT 1
+      FROM HealthDeclaration hd
+      WHERE hd.eid = e.eid
+        AND hd.date = d.date
+    )
+    GROUP BY e.eid
+    ORDER BY days DESC;
 END;
 $$ LANGUAGE plpgsql;
 
-
 ---- View Booking Report ----
 CREATE OR REPLACE FUNCTION view_booking_report
-RETURNS <type> AS $$
-DECLARE
-  temp INT := 0;
+  (IN start_date DATE, IN employee_id INTEGER)
+RETURNS TABLE
+  (floor_number INTEGER, room_number INTEGER, date DATE, start_hour TIME, is_approved BIT) AS $$
 BEGIN
-
-RETURN;
+  RETURN QUERY
+    SELECT s.floor, s.room, s.date, s.time, CASE
+      WHEN s.meid IS NOT NULL THEN B'1'
+      ELSE B'0' END
+    FROM Sessions s
+    WHERE s.beid = 1
+      AND s.date >= '2020-10-01'
+    ORDER BY s.date ASC, s.time ASC;
 END;
 $$ LANGUAGE plpgsql;
 
--- testing_purpose.sql
+++ testing_purpose.sql
@@ -22,45 +22,30 @@ INSERT INTO HealthDeclaration VALUES
 (0, '2021-10-04', 36.1, B'0'),
 (2, '2021-10-04', 36.1, B'0');
 
+INSERT INTO Booker VALUES (0), (1);
+
+INSERT INTO Manager VALUES (0);
+
+INSERT INTO MeetingRooms VALUES 
+(0, 0, 'MR_A', 0),
+(1, 1, 'MR_B', 0);
+
+INSERT INTO Sessions VALUES 
+(0, 0, '00:00:00', '2020-10-01', 0, 1),
+(1, 1, '01:01:01', '2020-10-01', NULL, 1),
+(1, 1, '02:02:02', '2020-10-02', 0, 1),
+(0, 0, '03:03:03', '2020-10-01', NULL, 1),
+(0, 0, '04:04:04', '2020-10-02', 0, 0);
+
+----------------------------------------------------------------
+---------------------------- OTHERS ----------------------------
+----------------------------------------------------------------
+
 SELECT non_compliance ('2021-10-01', '2021-10-04');
 
-SELECT DISTINCT hd.eid, COUNT(*)
-FROM HealthDeclaration hd
-GROUP BY hd.eid
-HAVING NOT EXISTS (
-  SELECT 1
-  FROM HealthDeclaration hd2
-  WHERE hd.eid = hd2.eid
-    AND hd2.date >= '2021-10-01'
-    AND hd2.date <= '2021-10-04'
-);
-
-SELECT DISTINCT hd.eid, COUNT(*)
-FROM HealthDeclaration hd
-GROUP BY hd.eid
-HAVING hd.eid NOT IN (
-  SELECT hd2.eid
-  FROM HealthDeclaration hd2
-  WHERE hd.eid = hd2.eid
-    AND hd2.date >= '2021-10-01'
-    AND hd2.date <= '2021-10-04'
-);
-
--- Cross join employees with list of dates
-SELECT e.eid
-FROM Employees e LEFT OUTER JOIN HealthDeclaration hd ON e.eid = hd.eid
-WHERE NOT EXISTS (
-  SELECT 1
-  FROM HealthDeclaration hd
-  WHERE hd.eid = e.eid 
-    AND hd.date >= '2021-10-01'
-    AND hd.date <= '2021-10-04'
-);
-
-
 WITH dates (date) AS (
-  SELECT DISTINCT date
-  FROM HealthDeclaration
+  SELECT *
+    FROM generate_series('2021-10-01', '2021-10-04', '1 day'::interval)
 )
 SELECT e.eid, COUNT(*)
 FROM Employees e, dates d
@@ -73,25 +58,15 @@ WHERE NOT EXISTS (
 GROUP BY e.eid;
 
 
+SELECT view_booking_report('2020-10-01', 1);
 
-WITH dates (date) AS (
-  SELECT date
-  FROM 
-)
-SELECT e.eid, COUNT(*)
-FROM Employees e, dates d
-WHERE NOT EXISTS (
-  SELECT 1
-  FROM HealthDeclaration hd
-  WHERE hd.eid = e.eid
-    AND hd.date = d.date
-)
-GROUP BY e.eid;
-
-
-
-
-
+SELECT floor, room, date, time, CASE
+  WHEN s.meid IS NULL THEN TRUE
+  ELSE FALSE END
+FROM Sessions s
+WHERE s.beid = 1
+  AND date >= '2020-10-01'
+ORDER BY date ASC, time ASC;
 
 
 
