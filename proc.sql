56e58dd25624be5588fd84fd5a2674e0a1dc8a2a
Lim Chek Jun <limchekjun@gmail.com>
2021-11-01 18:20:03 +0800

    Fix: retire date trigger wrong variable name

--- proc.sql
+++ proc.sql
@@ -460,9 +460,9 @@ RETURNS TRIGGER AS $$
 BEGIN
   IF EXISTS (SELECT *
       FROM Employees E
-      WHERE E.eid = NEW.eid AND E.resignation_date < NEW.date
+      WHERE E.eid = NEW.eid AND E.resigned_date < NEW.date
     ) THEN
-      RAISE NOTICE '% Employee has retired', NEW.eid;
+      RAISE NOTICE 'Employee % has retired', NEW.eid;
       RETURN NULL;
   ELSE
     RETURN NEW;
